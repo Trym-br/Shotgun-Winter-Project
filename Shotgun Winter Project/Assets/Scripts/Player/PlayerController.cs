8be6bd1b99e0a5c5d114efb8072014c7ce9238d4
Trym-br <trym.brandal.arhaug@gmail.com>
2024-12-06 15:16:37 +0100

    Bullet Drop and more Animations

--- Shotgun Winter Project/Assets/Scripts/Player/PlayerController.cs
+++ Shotgun Winter Project/Assets/Scripts/Player/PlayerController.cs
@@ -9,9 +9,12 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     private Camera _mainCam;
     private Vector3 _mousePos;
+    
+    private bool _isGrounded;
 
     #region GUN
-    [Header("GUN")] 
+    [Header("GUN")]
+    [SerializeField] private GameObject _cartrigdePrefab;
     [SerializeField] MagUIController _magUI;
     [SerializeField] private int _magSize = 8;
     // way to make it accessible as read only, but still formatted correctly in inspector (very hacky solution)
@@ -24,6 +27,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool _momentum;
     private float _fireTimer = 0f;
     public int _ammo { get; private set; } = 0;
+    private Transform _shotgunTransform;
     #endregion
     
     #region Animation
@@ -45,34 +49,68 @@ public class PlayerController : MonoBehaviour
        Upper_Body_Animator = transform.GetChild(0).GetComponent<Animator>();
        Lower_Body_Animator = transform.GetChild(1).GetComponent<Animator>();
        Shotgun_Animator = transform.GetChild(2).GetComponent<Animator>();
+       _shotgunTransform = transform.GetChild(2).transform;
     }
 
     private void Update()
     {
-        _mousePos = _mainCam.ScreenToWorldPoint(Input.mousePosition);
-        // Vector2 knockbackDir = new Vector2(_mousePos.y - transform.position.y, _mousePos.x - transform.position.x).normalized;
-        Vector2 knockbackDir = new Vector2(_mousePos.x - transform.position.x,
-                                           _mousePos.y - transform.position.y).normalized;
-        // print("Dir: " + knockbackDir + "\nM/T pos:\t" + _mousePos + "\t" + transform.position);
         // Shooting
         if (_inputActions.FirePressed && _ammo != 0 && _fireTimer < 0)
         {
-            Shotgun_Animator.SetTrigger("Shoot");
-            Upper_Body_Animator.SetTrigger("Flinch");
-            print("Shoot!");
-            // Resets momentum, makes it less cheesed but also very easy to maneuver, so maybe don't?
-            if(!_momentum){_rigidbody2D.linearVelocity = Vector2.zero;}
-            _rigidbody2D.AddForce(knockbackDir * (_blastForceModifier * -1), ForceMode2D.Impulse);
-            
-            _ammo--;
-            _magUI.UpdateMagUI(_ammo);
-            
+            Shoot();
             _fireTimer = _fireRate;
         }
         _fireTimer -= Time.deltaTime;
 
         // Scene Reload
         if (_inputActions.ResetPressed) { SceneManager.LoadScene(SceneManager.GetActiveScene().name); }
+    }
+
+    void Shoot()
+    {
+        // Calculates knockbackDirection
+        _mousePos = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 knockbackDir = new Vector2(_mousePos.x - transform.position.x,
+                                           _mousePos.y - transform.position.y).normalized;
+        // Starts Animations
+        Shotgun_Animator.SetTrigger("Shoot");
+        Upper_Body_Animator.SetTrigger("Flinch");
+        print("Shoot!");
+        
+        // Resets momentum, makes it less cheesed but also very easy to maneuver, so maybe don't?
+        if(!_momentum){_rigidbody2D.linearVelocity = Vector2.zero;}
+        _rigidbody2D.AddForce(knockbackDir * (_blastForceModifier * -1), ForceMode2D.Impulse);
+            
+        // Adjusts ammo / Mag
+        _ammo--;
+        _magUI.UpdateMagUI(_ammo);
+        
+        //Ejects cartridge
+        GameObject _ejectedCartridge = Instantiate(_cartrigdePrefab, _shotgunTransform.position, _shotgunTransform.rotation);
+        // print("Shotgun forward: " + _shotgunTransform.up);
+        // print("U/R|F: " + _shotgunTransform.up + " / " + _shotgunTransform.right + " / " + Vector3.Cross(_shotgunTransform.up, _shotgunTransform.right).normalized);
+        // Vector3 dir = (_shotgunTransform.up + _shotgunTransform.right*-4)/2f; 
+        Vector3 dir = (_shotgunTransform.up + _shotgunTransform.right*-1)/2f; 
+        _ejectedCartridge.GetComponent<Rigidbody2D>().linearVelocity = dir * 8f;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true; 
+            Lower_Body_Animator.SetBool("Grounded", true);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+           _isGrounded = false; 
+            Lower_Body_Animator.SetBool("Grounded", false);
+        }
     }
     
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
